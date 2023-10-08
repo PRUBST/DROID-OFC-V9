314a6bbc7efa2f35deb2d8debbe1bb76bc91c275
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-08 11:14:51 -0600

    Actualizar gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
-m.reply('[❗] WELCOME MESSAGE SUCCESSFULLY CONFIGURED FOR THIS GROUP')
+m.reply('[❗] Mensaje de bienvenida cnfigurado correctamente')
 } else throw `[❗] ENTER THE WELCOME MESSAGE YOU WANT TO ADD TO THIS:*\n*- @user (mention)*\n*- @group (group name)*\n*- @desc (group description)*`
 }
 handler.help = ['setwelcome <text>']

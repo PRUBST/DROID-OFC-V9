96f9a7418f693de430e4cb0eddb4d6f117d7e350
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-10 23:00:21 -0600

    Actualizar fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -11,45 +11,45 @@ let handler = async (_0xeb7f32, { text: _0x1ff34a, command: _0x3cba8e, args: _0x
     },
     'message': {
       'contactMessage': {
-        'vcard': 'BEGIN:VCARD\x0aVERSION:3.0\x0aN:Sy;Bot;;;\x0aFN:y\x0aitem1.TEL;waid=' + _0xeb7f32['sender']['split']('@')[0x0] + ':' + _0xeb7f32['sender']['split']('@')[0x0] + '\x0aitem1.X-ABLabel:Ponsel\x0aEND:VCARD'
+        'vcard': 'BEGIN:VCARD\x0aVERSION:3.0\x0aN:Sy;Bot;;;\x0aFN:y\x0aitem1.TEL;waid=' + _0xeb7f32.sender.split('@')[0] + ':' + _0xeb7f32.sender.split('@')[0] + '\x0aitem1.X-ABLabel:Ponsel\x0aEND:VCARD'
       }
     },
     'participant': '0@s.whatsapp.net'
   };
 
-  if (!_0x1ff34a) return conn['reply'](_0xeb7f32['chat'], 'Hola: ' + '@' + _0xeb7f32['sender'] + ' 😍 ¿Quieres chatear? Responde con el comando junto con tu mensaje.', _0xbfd118, _0xeb7f32);
+  if (!_0x1ff34a) return conn.reply(_0xeb7f32.chat, `Hola: @${_0xeb7f32.sender.split('@')[0]} 😍 ¿Quieres chatear? Responde con el comando junto con tu mensaje.`, _0xbfd118, _0xeb7f32);
 
   try {
     let _0x3d237d = await _0x560386('https://api.simsimi.net/v2/?text=' + _0x1ff34a + '&lc=es'),
-      _0x263de6 = await _0x3d237d['json']();
-    _0xeb7f32['reply'](_0x263de6['success']);
+      _0x263de6 = await _0x3d237d.json();
+    _0xeb7f32.reply(_0x263de6.success);
   } catch {
     try {
-      if (_0x1ff34a['includes']('Hola')) _0x1ff34a = _0x1ff34a['replace']('Hola', 'Hello');
-      if (_0x1ff34a['includes']('hola')) _0x1ff34a = _0x1ff34a['replace']('hola', 'Hello');
-      if (_0x1ff34a['includes']('HOLA')) _0x1ff34a = _0x1ff34a['replace']('HOLA', 'HELLO');
+      if (_0x1ff34a.includes('Hola')) _0x1ff34a = _0x1ff34a.replace('Hola', 'Hello');
+      if (_0x1ff34a.includes('hola')) _0x1ff34a = _0x1ff34a.replace('hola', 'Hello');
+      if (_0x1ff34a.includes('HOLA')) _0x1ff34a = _0x1ff34a.replace('HOLA', 'HELLO');
       let _0x12156f = await _0x560386('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + _0x1ff34a),
-        _0x4c57d7 = await _0x12156f['json']();
-      let senderTag = '@' + _0xeb7f32['sender']; // Obtiene la etiqueta @ junto con el nombre de usuario completo
-      let _0x200bd0 = await _0x560386('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + senderTag + '&msg=' + _0x4c57d7[0x0][0x0][0x0]),
-        _0xf1831a = await _0x200bd0['json']();
-      let _0x3b5505 = await _0x560386('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + _0xf1831a['cnt']),
-        _0xf07ea0 = await _0x3b5505['json']();
-      _0xeb7f32['reply'](_0xf07ea0[0x0][0x0][0x0]);
+        _0x4c57d7 = await _0x12156f.json();
+      let senderTag = `@${_0xeb7f32.sender.split('@')[0]}`; // Obtiene la etiqueta @ junto con el nombre de usuario
+      let _0x200bd0 = await _0x560386('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + senderTag + '&msg=' + _0x4c57d7[0][0][0]),
+        _0xf1831a = await _0x200bd0.json();
+      let _0x3b5505 = await _0x560386('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + _0xf1831a.cnt),
+        _0xf07ea0 = await _0x3b5505.json();
+      _0xeb7f32.reply(_0xf07ea0[0][0][0]);
     } catch {
       let _0x554e06 = await _0x560386('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=' + _0x1ff34a),
-        _0xa0735c = await _0x554e06['json']();
-      let _0x3d337f = await _0x560386('https://violetics.pw/api/utility/simsimi?apikey=beta&text=' + _0xa0735c[0x0][0x0][0x0]),
-        _0x397994 = await _0x3d337f['json']();
-      let _0x4a5cb0 = _0x397994['result'];
-      let _0x30135e = await _0x74f12c('' + _0x4a5cb0, { 'to': 'es', 'autoCorrect': !![] });
-      _0xeb7f32['reply'](_0x30135e['text']);
+        _0xa0735c = await _0x554e06.json();
+      let _0x3d337f = await _0x560386('https://violetics.pw/api/utility/simsimi?apikey=beta&text=' + _0xa0735c[0][0][0]),
+        _0x397994 = await _0x3d337f.json();
+      let _0x4a5cb0 = _0x397994.result;
+      let _0x30135e = await _0x74f12c('' + _0x4a5cb0, { to: 'es', autoCorrect: true });
+      _0xeb7f32.reply(_0x30135e.text);
     }
   }
 };
 
-handler['help'] = ['simi', 'bot']['map'](_0x532f0e => _0x532f0e + '\x20<teks>');
-handler['tags'] = ['fun'];
-handler['command'] = /^((sim)?simi|bot|alexa|cortana)$/i;
+handler.help = ['simi', 'bot'].map(_0x532f0e => `${_0x532f0e} <teks>`);
+handler.tags = ['fun'];
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
 
 export default handler;

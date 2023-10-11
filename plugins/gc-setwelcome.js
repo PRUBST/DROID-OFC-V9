0d262a295b53a8f71974548fe646d7d3a82e6804
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-11 14:02:41 -0600

    Actualizar gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -2,7 +2,28 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
 m.reply('[❗] Mensaje de bienvenida cnfigurado correctamente')
-} else throw `[❗] ENTER THE WELCOME MESSAGE YOU WANT TO ADD TO THIS:*\n*- @user (mention)*\n*- @group (group name)*\n*- @desc (group description)*`
+} else throw `[❗] Ingresa al mensaje de bienvenida. Ejemplo1:*\n*/setwelcome bienvenido al grupo @user*\n*Ejemplo2:\n*/setwelcome ╭•⋆҈͜͡.➣❂𝐃𝐑𝐎𝐈𝐃❂◄⋆҈͜͡•╮
+╽Bienvenid@ al grupo
+┃
+╿️@⁨user
+╿️🍄🍓🍄🍓🍄🍓🍄 
+├──────────⋆
+ ╿️.s | .audio/video | /menu3
+├──────────⋆
+┃  ⚠️ PROHIBIDO ⚠️
+├──────────⋆
+┃🚯 SPAM/ ENLACES 
+┃🚯 PORNO/ CP
+╿ 🚯GORE/ INSULTAR 
+╿
+├──────────⋆
+┃ 💦💦💦💦💦💦
+┃Lee la descripción
+╿️Del Grupo 💫💫💫
+╰•⋆҈͜͡.𝑶𝑭𝑪⋆𝑻𝑶𝑵𝒀⋆҈͜͡•╯
+
+🧑🏻‍💻️ *SÍGUEME EN TIKTOK* 😻
+http://tiktok.com/@tony.1p)*`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']

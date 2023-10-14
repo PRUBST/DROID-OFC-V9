f23bb223285ac993e37d80993870b8a9bd9e7b3e
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-14 11:11:52 -0600

    Actualizar Todos-Los-audios.js

--- plugins/Todos-Los-audios.js
+++ plugins/Todos-Los-audios.js
@@ -32,7 +32,7 @@ let vn = './Audios/baneado.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}    
    
-if (!chat.isBanned && chat.audios && m.text.match(/(si soy|SI SOY|Bebito Fiu Fiu|Bff)/gi)) {    
+if (!chat.isBanned && chat.audios && m.text.match(/(menu|Menú|MENU)/gi)) {    
 let vn = './Audios/bff.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}   

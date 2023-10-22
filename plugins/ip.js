3a6a81c33a0519acf175067ccfb3a72183120369
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-21 20:47:32 -0600

    Actualizar ip.js

--- plugins/ip.js
+++ plugins/ip.js
@@ -12,5 +12,5 @@ let handler = async (m, { conn, command, text }) => {
   
  handler.command = /^(Follar|violar)/i 
  handler.fail = null 
- handler.premium = true 
+ handler.register = true 
  export default handler

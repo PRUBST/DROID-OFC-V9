0e77cdeea8a98eb23ec0db744a111bf1e674727b
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-21 20:58:26 -0600

    Actualizar Anti-lista-enable.js

--- plugins/Anti-lista-enable.js
+++ plugins/Anti-lista-enable.js
@@ -1,1 +1,288 @@
-const handler=async(_0x15d694,{conn:_0x52b190,usedPrefix:_0x184b4b,command:_0x46159d,args:_0x163a35,isOwner:_0x5a673e,isAdmin:_0x4432da,isROwner:_0x4739fa})=>{const _0x423de4='To\x20see\x20the\x20instructions\x20go\x20to\x20the\x20menu\x20\x0a/Comangrupos'['trim'](),_0x1ba1f5=/true|enable|(turn)?on|1/i['test'](_0x46159d),_0x57dfe9=global['db']['data']['chats'][_0x15d694['chat']],_0x56cdb8=global['db']['data']['users'][_0x15d694['sender']],_0x1a4dfe=global['db']['data']['settings'][_0x52b190['user']['jid']]||{},_0x4f5896=(_0x163a35[0x0]||'')['toLowerCase']();let _0x58bade=![];const _0x1ab688=![];switch(_0x4f5896){case'welcome':if(!_0x15d694['isGroup']){if(!_0x5a673e){global['dfail']('group',_0x15d694,_0x52b190);throw![];}}else{if(!(_0x4432da||_0x5a673e||_0x4739fa)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['welcome']=_0x1ba1f5;break;case'detect':if(!_0x15d694['isGroup']){if(!_0x5a673e){global['dfail']('group',_0x15d694,_0x52b190);throw![];}}else{if(!_0x4432da){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['detect']=_0x1ba1f5;break;case'detect2':if(!_0x15d694['isGroup']){if(!_0x5a673e){global['dfail']('group',_0x15d694,_0x52b190);throw![];}}else{if(!_0x4432da){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['detect2']=_0x1ba1f5;break;case'simsimi':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['simi']=_0x1ba1f5;break;case'antiporno':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiporno']=_0x1ba1f5;break;case'delete':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['delete']=_0x1ba1f5;break;case'antidelete':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antidelete']=_0x1ba1f5;break;case'public':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}global['opts']['self']=!_0x1ba1f5;break;case'antilink':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiLink']=_0x1ba1f5;break;case'antilink2':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiLink2']=_0x1ba1f5;break;case'antiviewonce':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiviewonce']=_0x1ba1f5;break;case'modohorny':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['modohorny']=_0x1ba1f5;break;case'modoadmin':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['modoadmin']=_0x1ba1f5;break;case'autosticker':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['autosticker']=_0x1ba1f5;break;case'audios':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['audios']=_0x1ba1f5;break;case'restrict':_0x58bade=!![];if(!_0x5a673e){global['dfail']('owner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['restrict']=_0x1ba1f5;break;case'audios_bot':_0x58bade=!![];if(!_0x5a673e){global['dfail']('owner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['audios_bot']=_0x1ba1f5;break;case'nyimak':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}global['opts']['nyimak']=_0x1ba1f5;break;case'autoread':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['autoread2']=_0x1ba1f5,global['opts']['autoread']=_0x1ba1f5;break;case'pconly':case'privateonly':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}global['opts']['pconly']=_0x1ba1f5;break;case'gconly':case'grouponly':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}global['opts']['gconly']=_0x1ba1f5;break;case'swonly':case'statusonly':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}global['opts']['swonly']=_0x1ba1f5;break;case'anticall':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['antiCall']=_0x1ba1f5;break;case'antiprivado':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['antiPrivate']=_0x1ba1f5;break;case'modejadibot':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['modejadibot']=_0x1ba1f5;break;case'antispam':_0x58bade=!![];if(!_0x4739fa){global['dfail']('rowner',_0x15d694,_0x52b190);throw![];}_0x1a4dfe['antispam']=_0x1ba1f5;break;case'antitoxic':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiToxic']=_0x1ba1f5;break;case'antitraba':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiTraba']=_0x1ba1f5;break;case'antiarabes':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiArab']=_0x1ba1f5;break;case'antiarabes2':if(_0x15d694['isGroup']){if(!(_0x4432da||_0x5a673e)){global['dfail']('admin',_0x15d694,_0x52b190);throw![];}}_0x57dfe9['antiArab2']=_0x1ba1f5;break;default:if(!/[01]/['test'](_0x46159d))return await _0x52b190['sendMessage'](_0x15d694['chat'],{'text':_0x423de4},{'quoted':_0x15d694});throw![];}_0x52b190['sendMessage'](_0x15d694['chat'],{'text':'🔮𝐎𝐏𝐂𝐈𝐎𝐍:\x20'+_0x4f5896+'\x0a📊𝐄𝐒𝐓𝐀𝐃𝐎:\x20'+(_0x1ba1f5?'ACTIVADO':'DESACTIVADO')+'\x0a📣\x20𝐏𝐀𝐑𝐀:\x20'+(_0x58bade?'ESTE\x20BOT':_0x1ab688?'':'ESTE\x20CHAT')},{'quoted':_0x15d694});};handler['help']=['en','dis']['map'](_0x3f0474=>_0x3f0474+'able\x20<option>'),handler['tags']=['group','owner'],handler['command']=/^((en|dis)able|(tru|fals)e|(turn)?[01])$/i;export default handler;
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+let optionsFull = `╭─────( 🦆 )─────╮
+├➽  /enable antilink 
+├➽ /enable antilink2 
+├➽ /enable detect 
+├➽ /enable detect2
+├➽ /enable audios 
+├──────────⋆
+├➽ /enable antitoxic
+├➽ /enable antitraba
+├➽ /enable modoadmin  
+│───── ⋆⋅☆⋅⋆ ───── 
+│/enable *[para activar]*
+│/disable *[para desactivar]*
+│─── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ───
+│⬆️ *COMANDOS PARA GRUPOS*
+│           *SÓLO LOS ADMIN"S*
+│               *LO   PODRÁN* 
+│   *ACTIVAR/DESACTIVAR*
+ ╰─────────────╯`.trim()
+
+let isEnable = /true|enable|(turn)?on|1/i.test(command)
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[m.sender]
+let bot = global.db.data.settings[conn.user.jid] || {}
+let type = (args[0] || '').toLowerCase()
+let isAll = false, isUser = false
+switch (type) {
+case 'welcome':
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}
+} else if (!(isAdmin || isOwner || isROwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.welcome = isEnable
+break
+case 'detect':
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}
+} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.detect = isEnable
+break
+case 'detect2':
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}
+} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.detect2 = isEnable
+break    
+case 'simsimi':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.simi = isEnable
+break   
+case 'antiporno':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiporno = isEnable
+break        
+case 'delete':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.delete = isEnable
+break
+case 'antidelete':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.delete = !isEnable
+break
+case 'public':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+global.opts['self'] = !isEnable
+break
+case 'antilink':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiLink = isEnable
+break
+case 'antilink2':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiLink2 = isEnable 
+break
+case 'antiviewonce':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiviewonce = isEnable 
+break
+case 'modohorny':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.modohorny = isEnable          
+break
+case 'modoadmin':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.modoadmin = isEnable          
+break    
+case 'autosticker':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.autosticker = isEnable          
+break
+case 'audios':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.audios = isEnable          
+break
+case 'restrict':
+isAll = true
+if (!isOwner) {
+global.dfail('owner', m, conn)
+throw false
+}
+bot.restrict = isEnable
+break
+case 'nyimak':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+global.opts['nyimak'] = isEnable
+break
+case 'autoread':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.autoread2 = isEnable    
+global.opts['autoread'] = isEnable  
+break
+case 'pconly':
+case 'privateonly':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+global.opts['pconly'] = isEnable
+break
+case 'gconly':
+case 'grouponly':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+global.opts['gconly'] = isEnable
+break
+case 'swonly':
+case 'statusonly':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+global.opts['swonly'] = isEnable
+break
+case 'anticall':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.antiCall = isEnable
+break
+case 'antiprivado':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.antiPrivate = isEnable
+break
+case 'modejadibot':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.modejadibot = isEnable
+break     
+case 'antispam':
+isAll = true
+if (!isROwner) {
+global.dfail('rowner', m, conn)
+throw false
+}
+bot.antispam = isEnable    
+break
+case 'antitoxic':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiToxic = isEnable
+break
+case 'antitraba':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiTraba = isEnable
+break
+case 'antiarabes':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiArab = isEnable  
+break
+case 'antiarabes2':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiArab2 = isEnable  
+break    
+default:
+if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, { text: optionsFull }, { quoted: m })
+throw false
+}
+conn.sendMessage(m.chat, { text: `🧿𝐎𝐏𝐂𝐈𝐎𝐍: ${type}\n️🔮𝐄𝐒𝐓𝐀𝐃𝐎: ${isEnable ? 'ACTIVADO' : 'DESACTIVADO'}\n🗂️𝐏𝐀𝐑𝐀: ${isAll ? 'ESTE BOT' : isUser ? '' : 'ESTE CHAT'}` }, { quoted: m })        
+conn.sendMessage(m.chat, { text: `🧿 OPTION: ${type}\n️🔮 STATUS: ${isEnable ? 'ON' : 'OFF'}\n🗂️TO: ${isAll ? 'THIS BOT' : isUser ? '' : 'THIS CHAT'}` }, { quoted: m })        
+}
+handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.tags = ['group', 'owner']
+handler.command = /^((en|dis)able|(tru|fals)e|(turn)?[01])$/i
+handler.register = true
+export default handler

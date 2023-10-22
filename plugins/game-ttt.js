0778a98d7abdaf077481adf6bb76788b1247ad5b
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-21 20:28:16 -0600

    Actualizar game-ttt.js

--- plugins/game-ttt.js
+++ plugins/game-ttt.js
@@ -5,7 +5,7 @@ if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [ro
 if (!text) throw `*[❗] Required to name the games room*\n\n*—◉ Example*\n*◉ ${usedPrefix + command} New room*`
 let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
 if (room) {
-await m.reply('*[🕹️] Start the game, one player joined the game*')
+await m.reply('*[🕹️] Comienza el juego, un jugador se unió al juego.*')
 room.o = m.chat
 room.game.playerO = m.sender
 room.state = 'PLAYING'
@@ -24,7 +24,7 @@ O: '⭕',
 9: '9️⃣',
 }[v]})
 let str = `
-🎮 𝐓𝐇𝐑𝐄𝐄 𝐈𝐍 𝐀 𝐑𝐎𝐖 🎮
+🎮 3 EN UNA FILA 🎮
 
 ❎ = @${room.game.playerX.split('@')[0]}
 ⭕ = @${room.game.playerO.split('@')[0]}
@@ -46,8 +46,8 @@ game: new TicTacToe(m.sender, 'o'),
 state: 'WAITING' }
 if (text) room.name = text     
 let imgplay = `https://cope-cdnmed.agilecontent.com/resources/jpg/8/9/1590140413198.jpg`
-conn.reply(m.chat, `*🕹 𝐓𝐑𝐄𝐄 𝐈𝐍 𝐀 𝐑𝐎𝐖 🎮*\n\n*◉ Waiting for the second player*\n*◉ To delete or exit the game use the command ${usedPrefix}delttt*\n\n◉ To join the enter game: (${usedPrefix + command} ${text})`, m)
-//conn.sendButton(m.chat, `*🕹 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮*\n\n*◉ 𝙴𝚂𝙿𝙴𝚁𝙰𝙽𝙳𝙾 𝙰𝙻 𝚂𝙴𝙶𝚄𝙽𝙳𝙾 𝙹𝚄𝙶𝙰𝙳𝙾𝚁*\n*◉ 𝙿𝙰𝚁𝙰 𝙱𝙾𝚁𝚁𝙰𝚁 𝙾 𝚂𝙰𝙻𝙸𝚁𝚂𝙴 𝙳𝙴 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝚄𝚂𝙴𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}delttt*`, wm, imgplay, [['𝚄𝙽𝙸𝚁𝚂𝙴 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
+conn.reply(m.chat, `*🕹 𝐓𝐑𝐄𝐄 𝐈𝐍 𝐀 𝐑𝐎𝐖 🎮*\n\n*◉ Esperando al segundo jugador.\n Para borrar o salir del juego, usa el comando correspondiente. ${usedPrefix}delttt*\n\n◉ To join the enter game: (${usedPrefix + command} ${text})`, m)
+//conn.sendButton(m.chat, `*🕹 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮*\n\n*◉ Esperando al segundo jugador.\n Para borrar o salir del juego, usa el comando correspondiente. ${usedPrefix}delttt*`, wm, imgplay, [['𝚄𝙽𝙸𝚁𝚂𝙴 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
 conn.game[room.id] = room
 }}
 handler.command = /^(tictactoe|ttc|ttt|xo)$/i

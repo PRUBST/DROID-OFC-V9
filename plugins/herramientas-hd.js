22030c3c132c19dce20bb6c1fbcdff2ce8c6e207
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-10-24 13:55:31 -0600

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -4,19 +4,20 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `[❗] Lo siento, pero actualmente no puedo enviar imágenes ni procesar comandos que involucren envío de imágenes. ¿En qué más puedo ayudarte? ${usedPrefix + command}`;
+  if (!mime) throw `[❗] send an image and then send an image and then place the command ${usedPrefix + command}`;
   if (!/image\/(jpe?g|png)/.test(mime)) throw `[❗] the file format (${mime}) it was not possible to send tag to an image `;
-  m.reply("[❗] _*Cargando...*_ );
+  m.reply("[❗] processing");
   let img = await q.download?.();
   let pr = await remini(img, "enhance");
   conn.sendMessage(m.chat, {image: pr}, {quoted: m});
  } catch {
-  throw "❌ Occurrio un error ❌";
+  throw "❌an error occurred try again❌";
  }
 };
 handler.help = ["remini", "hd", "enhance"];
 handler.tags = ["ai", "tools"];
 handler.command = ["remini", "hd", "enhance"];
+handler.register = true
 handler.register = true
 export default handler;
 

13565a6ad97c7d4b26ccda6f70bd3ec444a4690d
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-03 23:41:32 -0600

    Actualizar Sticker-kiss.js

--- plugins/Sticker-kiss.js
+++ plugins/Sticker-kiss.js
@@ -12,7 +12,14 @@ let handler = async (_0x734089, { conn: _0x1952bd }) => {
     let { url: _0x1309b9 } = _0x16385f;
 
     let senderName = _0x1952bd.getName(_0x734089.sender);
-    let _0x20fedb = await sticker(null, _0x1309b9, senderName + '\x20le\x20dio\x20besos\x20a\x20' + _0x734089['mentionedJid']['map'](_x34d5f8 => _x34d5f8 === _0x734089['sender'] ? 'alguien\x20' : _0x1952bd.getName(_x34d5f8) + '\x20le\x20dio\x20besos\x20a\x20')['join'](',\x20'));
+
+    let mentionedNames = _0x734089['mentionedJid']
+      .map(_x34d5f8 => _x34d5f8 === _0x734089['sender'] ? 'alguien\x20' : _0x1952bd.getName(_x34d5f8))
+      .join(',\x20');
+
+    let message = senderName + (mentionedNames.length > 0 ? '\x20le dio besos a ' + mentionedNames : '\x20te dio besos');
+
+    let _0x20fedb = await sticker(null, _0x1309b9, message);
 
     _0x1952bd['sendFile'](_0x734089['chat'], _0x20fedb, null, { 'asSticker': !![] });
   } catch (_0x44f6c6) {}

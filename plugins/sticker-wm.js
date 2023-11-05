293eef9ad4d28759b6c97f383b8cf2b085f7d476
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-04 20:55:30 -0600

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,26 +1,27 @@
+
 import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!m.quoted) return conn.reply(m.chat, `[❗] Please label the sticker you want to steal and enter the name you want to give to the sticker.`, fkontak, m)
-let stiker = false
-try {
-let [packname, ...author] = text.split('|')
-author = (author || []).join('|')
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) return conn.reply(m.chat, `[❗] Respond to a sticker you want to steal and enter a name`, fkontak, m)
-let img = await m.quoted.download()
-if (!img) return conn.reply(m.chat, `[❗]Respond to a sticker you want to steal and enter the name you want to give to the sticker`, fkontak, m)
-stiker = await addExif(img, packname || global.igfg, author || global.author)
-} catch (e) {
-console.error(e)
-if (Buffer.isBuffer(e)) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else return conn.reply(m.chat, `[❗] sorry an error occurred`, fkontak, m)
-}}
-handler.help = ['wm <packname>|<author>']
+let handler = async (m, { conn, text, args }) => {
+  if (!m.quoted) throw '🧑🏻‍💻 Responde a un sticker'
+  let stiker = false
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+  try {
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw '🧑🏻‍💻 Responde a un sticker'
+    let img = await m.quoted.download()
+    if (!img) throw '🧑🏻‍💻 Responde a un sticker!'
+    stiker = await addExif(img, f, g)
+  } catch (e) {
+    console.error(e)
+    if (Buffer.isBuffer(e)) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null, rpl)
+     else throw '📵 La conversión falló'
+  }
+}
+handler.help = ['take <nombre>|<autor>']
 handler.tags = ['sticker']
-handler.command = /^takewm$/i
-handler.register = true
-handler.premium = true
+handler.command = ['take', 'wm'] 
+
 export default handler

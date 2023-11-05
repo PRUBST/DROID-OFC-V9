92ae5823be7a8d2f0d764175584660e7f814f4b8
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-04 19:38:30 -0600

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,17 +1,23 @@
-let handler = async (m, { conn, text}) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) return conn.reply(m.chat, `[❗]ENTER THE @tag OF SOME USER`, fkontak, m)
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-if (!who) return conn.reply(m.chat, `taguea al usuario que de sea desbanear: Ejemplo: /unban @tag`, fkontak, m)
-let users = global.db.data.users
-users[who].banned = false
-conn.reply(m.chat, `👨🏻‍💻 *USUARIO DESBANEADO,\n YA PUEDE USARME:*`, fkontak, m)
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+   let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = global.db.data.users[who]
+    if (!who) throw `🧑🏻‍💻️ Etiqueta o menciona al usuario para desbanear`
+    let users = global.db.data.users
+    users[who].banned = false
+    conn.reply(m.chat, `
+🌷 DESBANEADO 🌟
+
+⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆
+@${who.split`@`[0]} 🧑🏻‍💻 ha sido desbaneado`, m, { mentions: [who] })
 }
-handler.help = ['unbanuser']
+handler.help = ['unban @user']
 handler.tags = ['owner']
 handler.command = /^unban$/i
-handler.register = true
 handler.admin = true
+handler.rowner = false
+
 export default handler

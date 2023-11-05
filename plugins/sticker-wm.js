252171761ce9f7b460fac7252a05e5c560aa85fa
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-04 21:10:22 -0600

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,27 +1,25 @@
-
 import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text, args }) => {
-  if (!m.quoted) throw '🧑🏻‍💻 Responde a un sticker'
-  let stiker = false
-       let stick = args.join(" ").split("|");
-       let f = stick[0] !== "" ? stick[0] : packname;
-       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
-  try {
-    let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw '🧑🏻‍💻 Responde a un sticker'
-    let img = await m.quoted.download()
-    if (!img) throw '🧑🏻‍💻 Responde a un sticker!'
-    stiker = await addExif(img, f, g)
-  } catch (e) {
-    console.error(e)
-    if (Buffer.isBuffer(e)) stiker = e
-  } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null, rpl)
-     else throw '📵 La conversión falló'
-  }
-}
-handler.help = ['take <nombre>|<autor>']
+let handler = async (m, { conn, text }) => {
+if (!m.quoted) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝚄𝙽 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]RESPOND TO A STICKER YOU WANT TO SAVE IN A PACK AND A OR NAME*'
+let stiker = false
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝚄𝙽 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴\n\n\n*[❗𝐈𝐍𝐅𝐎❗]RESPOND TO A STICKER YOU WANT TO SAVE IN A PACK AND A OR NAME*'
+let img = await m.quoted.download()
+if (!img) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝚄𝙽 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]RESPOND TO A STICKER YOU WANT TO SAVE IN A PACK AND A OR NAME*'
+stiker = await addExif(img, packname || global.packname, author || global.author)
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙰𝙻𝙶𝙾 𝙵𝙰𝙻𝙻𝙾.. 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙷𝙰𝚈𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙸𝙳𝙾 𝙰 𝚄𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚈 𝙷𝙰𝚈𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝙳𝙾 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
+}}
+handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
-handler.command = ['take', 'wm'] 
-
+handler.command = /^take|robarsticker|wm$/i
+handler.register = true
+handler.limit = 1
 export default handler

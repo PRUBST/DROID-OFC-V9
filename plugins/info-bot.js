a7a1fbb116b103e90f913c2641308c5025020daf
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-10 15:08:46 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.all = async function (m) {
 let vn = './Audios/bot.mp3'
 let chat = global.db.data.chats[m.chat]
-if (/^hola, ¿qué funciones tienes?$/i.test(m.text) && !chat.isBanned) { 
+if (/^hola,\x20¿qué\x20funciones\x20tienes?$/i.test(m.text) && !chat.isBanned) { 
 conn.sendPresenceUpdate('recording', m.chat)   
 await m.reply(`*Hola, mis funciones incluyen la creación de stickers, descarga de música, y entre otras más.\n\n\nRecuerda que cada comando que pones debe ir primero un punto y luego comando. Ejemplo:
 /s\n\nSímbolos admitidos: * / ! " .  @ + × ^

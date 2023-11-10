76cfb96efed3a2dcb67364f794587917cc314e85
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-10 15:07:42 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,12 +1,18 @@
-import _0x5c69e8 from'fs';let handler=_0x3b0305=>_0x3b0305;handler['all']=async function(_0x387867){let _0x43c12c={'key':{'participants':'0@s.whatsapp.net','remoteJid':'status@broadcast','fromMe':![],'id':'Halo'},'message':{'contactMessage':{'vcard':'BEGIN:VCARD\x0aVERSION:3.0\x0aN:Sy;Bot;;;\x0aFN:y\x0aitem1.TEL;waid='+_0x387867['sender']['split']('@')[0x0]+':'+_0x387867['sender']['split']('@')[0x0]+'\x0aitem1.X-ABLabel:Ponsel\x0aEND:VCARD'}},'participant':'0@s.whatsapp.net'},_0x1b4da8='./lib/img/miku.mp3',_0x34fc6b=global['db']['data']['chats'][_0x387867['chat']];return/^Hola, ¿qué funciones tienes?$/i['test'](_0x387867['text'])&&!_0x34fc6b['isBanned']&&(conn['sendPresenceUpdate']('recording',_0x387867['chat']),await conn['reply'](_0x387867['chat'],'Hola, mis funciones incluyen la creación de stickers, descarga de música, y entre otras más.
-
-Recuerda que cada comando que pones debe ir primero un punto y el comando. Ejemplo:
-/s
-
-Símbolos admitidos: * / ! " .  @ + × ^
-
-También registrate para que puedas ver todas mis funciones y usarlas. Espero serte útil.
-
-registrate escribiendo: */reg nombre.edad*
-ejemplo: */reg droid.17*
-',_0x43c12c,_0x387867),conn['sendMessage'](_0x387867['chat'],{'audio':{'url':_0x1b4da8},'fileName':'error.mp3','mimetype':'audio/mp4','seconds':'4556','ptt':!![]},{'quoted':_0x387867})),!0x0;};export default handler;
+import fs from "fs"
+let handler = m => m
+handler.all = async function (m) {
+let vn = './Audios/bot.mp3'
+let chat = global.db.data.chats[m.chat]
+if (/^hola, ¿qué funciones tienes?$/i.test(m.text) && !chat.isBanned) { 
+conn.sendPresenceUpdate('recording', m.chat)   
+await m.reply(`*Hola, mis funciones incluyen la creación de stickers, descarga de música, y entre otras más.\n\n\nRecuerda que cada comando que pones debe ir primero un punto y luego comando. Ejemplo:
+/s\n\nSímbolos admitidos: * / ! " .  @ + × ^
+\nTambién registrate para que puedas ver todas mis funciones y usarlas. Espero serte útil.
+\nregistrate escribiendo: */reg nombre.edad*
+ejemplo: */reg droid.17*`)
+conn.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', seconds: '4556', ptt: true }, { quoted: m })
+}
+return !0
+}
+handler.register = true
+export default handler

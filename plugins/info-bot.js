bfef837ff6f35a86467e41e2c4a20b6c9495c648
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-10 15:19:59 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -2,10 +2,10 @@ import fs from "fs"
 let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
-if (/^sticker|funciones|ayuda$/i.test(m.text) && !chat.isBanned) { 
+if (/^sticker|funciones|stiker$/i.test(m.text) && !chat.isBanned) { 
 conn.sendPresenceUpdate('recording', m.chat)   
-await m.reply(`*Hola, mis funciones incluyen la creación de stickers, descarga de música, y entre otras más.\n\n\nRecuerda que cada comando que pones debe ir primero un punto y luego comando. Ejemplo:
-/s\n\nSímbolos admitidos: * / ! " .  @ + × ^
+await m.reply(`*Hola, mis funciones incluyen la creación de stickers, descargar música/vídeos, y entre otras más.*\n\n\nRecuerda que cada comando que pones debe ir primero un punto y luego el comando. Ejemplo:
+ */s*\n\nSímbolos admitidos: * / ! " .  @ + × ^
 \nTambién registrate para que puedas ver todas mis funciones y usarlas. Espero serte útil.
 \nregistrate escribiendo: */reg nombre.edad*
 ejemplo: */reg droid.17*`)
@@ -13,5 +13,4 @@ conn.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 
 }
 return !0
 }
-handler.register = true
 export default handler

795e6c158fe03cdd13ad7bdf93825457f23a04fa
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-18 22:08:53 -0600

    Actualizar rpg-perfil.j

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,21 +1,26 @@
 import { createHash } from 'crypto';
 
 let handler = async (m, { conn, usedPrefix, participants, isPrems }) => {
-  let pp = 'https://i.imgur.com/HE1dWt6.png';
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
 
-  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`;
+  if (!(who in global.db.data.users)) {
+    // Si no encuentra el perfil, envía la imagen predeterminada
+    let ppDefault = 'https://i.imgur.com/HE1dWt6.png';
+    return conn.sendMessage(m.chat, { image: { url: ppDefault } }, 'extendedTextMessage', { quoted: m });
+  }
 
+  let pp;
   try {
     pp = await conn.getProfilePicture(who);
   } catch (e) {
     // Maneja la excepción si la obtención de la imagen falla
   } finally {
-    let { name, role, role2, level, limit, money, exp, joincount, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who];
-    let username = conn.getName(who);
-    let prem = global.prems.includes(who.split `@`[0]);
-    let sn = createHash('md5').update(who).digest('hex');
-    let str = `╭「➻❥DROID-8-MD➻❥」
+    if (pp) {
+      let { name, role, role2, level, limit, money, exp, joincount, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who];
+      let username = conn.getName(who);
+      let prem = global.prems.includes(who.split `@`[0]);
+      let sn = createHash('md5').update(who).digest('hex');
+      let str = `╭「➻❥DROID-8-MD➻❥」
 │➯ *𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ' : ''}
 │➯ *link:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
 │➯ *⚓Rango:* ${role}
@@ -31,7 +36,10 @@ let handler = async (m, { conn, usedPrefix, participants, isPrems }) => {
 │➯ *${sn}*
 ╰───────────────╯`;
 
-    conn.sendMessage(m.chat, { image: { url: pp }, caption: str }, 'extendedTextMessage', { quoted: m });
+      return conn.sendMessage(m.chat, { image: { url: pp } }, 'extendedTextMessage', { quoted: m });
+    } else {
+      throw 'No se pudo encontrar la imagen de perfil.';
+    }
   }
 };
 

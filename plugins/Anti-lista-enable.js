18439cff2320a7c421f0304b18a8b8ce39c37565
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-18 21:20:52 -0600

    Actualizar Anti-lista-enable.js

--- plugins/Anti-lista-enable.js
+++ plugins/Anti-lista-enable.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🐌 Velocidad : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`${ssd}🚀 Velocidad : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']

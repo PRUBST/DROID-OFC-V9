69f52fde2cbfa7279ecd36dfbdd5e82e223eea78
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-18 21:42:13 -0600

    Actualizar rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,21 +1,26 @@
 import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
+
 let handler = async(m, { conn, usedPrefix, participants, isPrems }) => {
-let pp = 'https://i.imgur.com/HE1dWt6.png'
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`
-try {
-pp = await conn.profilePictureUrl(who)
-} catch (e) {
-} finally {
-let { name, role, role2, level, limit, money, exp, joincount, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
-let username = conn.getName(who)
-let prem = global.prems.includes(who.split `@` [0])
-let sn = createHash('md5').update(who).digest('hex')
-let str = `╭「➻❥DROID-8-MD➻❥」
+  let pp = 'https://i.imgur.com/HE1dWt6.png'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`
+
+  try {
+    let ppBuffer = await conn.getProfilePicture(who)
+    pp = `data:image/jpeg;base64,${ppBuffer.toString('base64')}`
+  } catch (e) {
+    // Maneja la excepción si la obtención de la imagen falla
+  } finally {
+    let { name, role, role2, level, limit, money, exp, joincount, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
+    let username = conn.getName(who)
+    let prem = global.prems.includes(who.split `@` [0])
+    let sn = createHash('md5').update(who).digest('hex')
+    let str = `╭「➻❥DROID-8-MD➻❥」
 │➯ *𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-│➯ *link:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
+│➯ *link:* wa.me/${who.split`@`[0]}${registered ? '\n*EDAD:* ' + age + ' años' : ''}
 │➯ *⚓Rango:* ${role}
 │➯ *📊Poder:* ${role2}
 │➯ *✳️️Nivel:* ${level}
@@ -28,10 +33,12 @@ let str = `╭「➻❥DROID-8-MD➻❥」
 │➯ *📝número de serie:* 
 │➯ *${sn}*
 ╰───────────────╯`
-conn.sendMessage(m.chat, { image: { url: pp }, caption: str }, { quoted: m })
-//conn.sendButton(m.chat, str, author, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)
-}}
+    conn.sendMessage(m.chat, { image: { url: pp }, caption: str }, { quoted: m })
+  }
+}
+
 handler.help = ['profile [@user]']
 handler.tags = ['xp']
 handler.command = /^perfil|profile?$/i
+
 export default handler

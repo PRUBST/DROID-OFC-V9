8d2f8e60dc9c5c2856099f99b30c643a8c6f2e95
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2024-03-18 18:08:55 -0600

    Actualizar jadibts.js

--- plugins/jadibts.js
+++ plugins/jadibts.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, text }) => {
   if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*` 
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender 
 
-  // Mensaje con reacción
+  // Mensaje con reacción incluida
   await conn.sendMessage(m.chat, {
     text: `
  @!${text} 
@@ -16,12 +16,6 @@ let handler = async (m, { conn, command, text }) => {
       tag: text
     },
     contextInfo: { mentionedJid: [user] },
-    reaction: { text: '👍', key: '👍' } // Aquí está el código de reacción con emoji
+    reaction: { text: '👍', key: '👍' } // Reacción incluida en el mensaje
   });
-
 } 
-
-handler.command = /^(PRUEBA3|PRUEBA3)/i 
-handler.fail = null 
-handler.register = true 
-export default handler

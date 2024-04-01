2fc475420b8d66865b8add546f6e1819c85a8f05
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2024-03-31 23:15:59 -0600

    Actualizar autoresponder.js

---
+++ plugins/autoresponder.js
@@ -0,0 +1,14 @@
+import { sticker } from '../lib/sticker.js'
+let handler = m => m
+
+handler.all = async function (m, {conn}) {
+let chat = global.db.data.chats[m.chat]
+
+if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
+let stiker = await sticker(imagen1, false, global.packname, global.author)  
+this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: 'TU PUTA MADRE', body: '🤓☝️', sourceUrl: `https://chat.whatsapp.com/L4cpzEr1wGOAEVdWeWBMn7`, thumbnail: imagen2}}})}
+
+return !0 }
+handler.register = true
+export default handler

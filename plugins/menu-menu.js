094a67978d0f63570d40fd3baf010132a83ebc95
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2024-03-31 19:34:22 -0600

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-        let img = (await axios.get(`https://raw.githubusercontent.com/fgmods/fg-team/main/img/hu.json`)).data
+        let img = (await axios.get(`https://postimg.cc/gallery/2Rmmqm2g`)).data
     let stiker = await sticker(null, global.API(`${pickRandom(img)}`), global.packname, global.author)
     if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', { asSticker: true }, m)
     throw stiker.toString()   
